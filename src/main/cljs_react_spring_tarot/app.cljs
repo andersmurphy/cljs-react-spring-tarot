35123c0165ac7245dfbc8462f81471a21299825a
andersmurphy <andersmurphy@gmail.com>
2021-07-30 22:57:11 +0100

    Set up helix

--- src/main/cljs_react_spring_tarot/app.cljs
+++ src/main/cljs_react_spring_tarot/app.cljs
@@ -1,4 +1,22 @@
-(ns cljs-react-spring-tarot.app)
+(ns cljs-react-spring-tarot.app
+  (:require [helix.core :refer [defnc $]]
+            [helix.hooks :as hooks]
+            [helix.dom :as d]
+            [react-dom :as rdom]))
+
+(defnc greeting
+  "A component which greets a user."
+  [{:keys [name]}]
+  (d/div "Hello, " (d/strong name) "!"))
+
+(defnc app []
+  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
+    (d/div
+     (d/h1 "Welcome!")
+     ($ greeting {:name (:name state)})
+     (d/input {:value (:name state)
+               :on-change
+               #(set-state assoc :name (.. % -target -value))}))))
 
 (defn init []
-  (println "Hello World"))
+  (rdom/render ($ app) (js/document.getElementById "app")))
