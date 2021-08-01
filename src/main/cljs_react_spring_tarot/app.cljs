ae7a5aa59fdd832554cb8ee526519646125e6ad0
andersmurphy <andersmurphy@gmail.com>
2021-08-01 11:02:46 +0100

    Migrate to latest spring/gestures dependencies

--- src/main/cljs_react_spring_tarot/app.cljs
+++ src/main/cljs_react_spring_tarot/app.cljs
@@ -2,19 +2,19 @@
   (:require [helix.core :refer [defnc $]]
             [react-dom :as rdom]
             [helix.hooks :as hks]
-            [react-spring :as spr]
+            ["@react-spring/web" :as spr]
             [react-use-gesture :as gest]))
 
 (defn use-springs [n f]
-  (let [[props set] (-> (spr/useSprings n (comp clj->js f))
+  (let [[props api] (-> (spr/useSprings n (comp clj->js f))
                         (js->clj :keywordize-keys true))]
-    [props (fn [f] (set (comp clj->js f)))]))
+    [props api]))
 
 (defn to [[x y] f]
-  (spr/interpolate #js [x y] f))
+  (spr/to #js [x y] f))
 
-(defn use-gesture [f]
-  (gest/useGesture (comp f #(js->clj % :keywordize-keys true))))
+(defn use-drag [f]
+  (gest/useDrag (comp f #(js->clj % :keywordize-keys true))))
 
 (def cards
   ["https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg" "https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg" "https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg" "https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg" "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg" "https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg"])
@@ -24,49 +24,54 @@
 
 (defnc app []
   (let [[gone set-gone] (hks/use-state #{})
-        [props set] (use-springs
-                     (count cards)
-                     (fn [i]
-                       {:x 0
-                        :y (* i -4)
-                        :scale 1
-                        :rot (- (* (rand) 20) 10)
-                        :delay (* i 100)
-                        :from {:x 0 :y -1000 :scale 1.5 :rot 0}}))
+        [props api]
+        (use-springs
+         (count cards)
+         (fn [i]
+           {:x 0
+            :y (* i -4)
+            :scale 1
+            :rot (- (* (rand) 20) 10)
+            :delay (* i 100)
+            :from {:x 0 :y -1000 :scale 1.5 :rot 0}}))
         bind
-        (use-gesture
-         (fn [{[index]  :args
-               [xDelta] :delta
+        (use-drag
+         (fn [{[index] :args
+               [mx]    :movement
                [xDir]  :direction
-               :keys [down velocity]}]
+               :keys [active velocity] :as m}]
            (let [trigger (> velocity 0.2)
-                 dir (if (< xDir 0) -1 1)
-                 gone (if (and (not down) trigger) (conj gone index) gone)]
-             (set
+                 gone (if (and (not active) trigger) (conj gone index) gone)]
+             (.start
+              api
               (fn [i]
                 (when (= i index)
-                  {:x (if (gone index)
-                        (* (+ 200 (.-innerWidth js/window)) dir)
-                        (if down xDelta 0))
-
-                   :rot (+ (/ xDelta 100)
-                           (if (gone i) (* dir 10 velocity) 0))
-                   :scale (if down 1.1 1)
-                   :config {:friction 50
-                            :tension (if down
-                                       800
-                                       (if (gone i) 200 500))}})))
+                  (clj->js
+                   {:x (if (gone i)
+                         (* (+ 200 (.-innerWidth js/window)) xDir)
+                         (if active mx 0))
+                    :rot (+ (/ mx 100)
+                            (if (gone i) (* xDir 10 velocity) 0))
+                    :scale (if active 1.1 1)
+                    :delay nil
+                    :config {:friction 50
+                             :tension (if active
+                                        800
+                                        (if (gone i) 200 500))}}))))
              (set-gone gone)
-             (when (and (not down) (= (count gone) (count cards)))
+             (when (and (not active) (= (count gone) (count cards)))
                (js/setTimeout
                 (fn []
                   (set-gone #{})
-                  (set (fn [i]
-                         {:x 0
-                          :y (* i -4)
-                          :scale 1
-                          :rot (- (* (rand) 20) 10)
-                          :delay (* i 100)})))
+                  (.start
+                   api
+                   (fn [i]
+                     (clj->js
+                      {:x 0
+                       :y (* i -4)
+                       :scale 1
+                       :rot (- (* (rand) 20) 10)
+                       :delay (* i 100)}))))
                 600)))))]
     (map-indexed
      (fn [i {:keys [x y rot scale]}]
