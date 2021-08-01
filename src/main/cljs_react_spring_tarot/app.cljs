7822cb88fcbc5389d0fd91a1ac5d50160febd978
andersmurphy <andersmurphy@gmail.com>
2021-08-01 22:21:41 +0100

    Use shorthand props for tranforms

--- src/main/cljs_react_spring_tarot/app.cljs
+++ src/main/cljs_react_spring_tarot/app.cljs
@@ -8,11 +8,7 @@
 (defn use-springs [n f]
   (let [[props api] (-> (spr/useSprings n (comp clj->js f))
                         (js->clj :keywordize-keys true))]
-
     [props {:start! (fn [f] (.start api (comp clj->js f)))}]))
-
-(defn to [[x y] f]
-  (spr/to #js [x y] f))
 
 (defn use-drag
   ([f] (use-drag f {}))
@@ -22,8 +18,8 @@
 (def cards
   ["https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg" "https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg" "https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg" "https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg" "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg" "https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg"])
 
-(defn trans [r s]
-  (str "perspective(1500px) rotateX(30deg) rotateY("(/ r 10)"deg) rotateZ("r"deg) scale("s")"))
+(defn url [url]
+  (str "url("url")"))
 
 (defnc app []
   (let [[gone set-gone!] (hks/use-state #{})
@@ -34,7 +30,7 @@
            {:x 0
             :y (* i -4)
             :scale 1
-            :rot (- (* (rand) 20) 10)
+            :rotateZ (- (* (rand) 20) 10)
             :delay (* i 100)
             :from {:x 0 :y -1000 :scale 1.5 :rot 0}}))
         bind
@@ -52,8 +48,8 @@
                   {:x (if (gone i)
                         (* (+ 200 (.-innerWidth js/window)) dir)
                         (if down mx 0))
-                   :rot (+ (/ mx 100)
-                           (if (gone i) (* dir 10 velocity) 0))
+                   :rotateZ (+ (/ mx 100)
+                               (if (gone i) (* dir 10 velocity) 0))
                    :scale (if down 1.1 1)
                    :delay nil
                    :config {:friction 50
@@ -70,21 +66,22 @@
                      {:x 0
                       :y (* i -4)
                       :scale 1
-                      :rot (- (* (rand) 20) 10)
+                      :rotateZ (- (* (rand) 20) 10)
                       :delay (* i 100)})))
                 600))))
          {:axis "x"})]
     (map-indexed
-     (fn [i {:keys [x y rot scale]}]
+     (fn [i {:keys [x y rot scale rotateZ]}]
+       (js/console.log rot)
        ($ spr/animated.div
           {:key i
            :style
-           #js {:transform (to [x y] (fn [x y]
-                                       (str "translate3d("x"px,"y"px,0)")))}}
+           #js {:x x :y y}}
           ($ spr/animated.div
              {:style
-              #js {:transform       (to [rot scale] trans)
-                   :backgroundImage (str "url("(cards i)")")
+              #js {:rotateZ rotateZ
+                   :scale scale
+                   :backgroundImage (url (cards i))
                    :touchAction "pan-y"}
               & (bind i)})))
      props)))
