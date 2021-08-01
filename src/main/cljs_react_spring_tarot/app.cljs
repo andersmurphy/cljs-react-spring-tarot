b6d7af7e0a3254bd79b93c7e5411b1cd61207f6a
andersmurphy <andersmurphy@gmail.com>
2021-08-01 13:31:56 +0100

    Streamline api wrapper

--- src/main/cljs_react_spring_tarot/app.cljs
+++ src/main/cljs_react_spring_tarot/app.cljs
@@ -8,14 +8,16 @@
 (defn use-springs [n f]
   (let [[props api] (-> (spr/useSprings n (comp clj->js f))
                         (js->clj :keywordize-keys true))]
-    [props api]))
+
+    [props {:start! (fn [f] (.start api (comp clj->js f)))}]))
 
 (defn to [[x y] f]
   (spr/to #js [x y] f))
 
-(defn use-drag [f & [config]]
-  (gest/useDrag (comp f #(js->clj % :keywordize-keys true))
-                (or config {})))
+(defn use-drag
+  ([f] (use-drag f {}))
+  ([f config]
+   (gest/useDrag (comp f #(js->clj % :keywordize-keys true)) config)))
 
 (def cards
   ["https://upload.wikimedia.org/wikipedia/en/f/f5/RWS_Tarot_08_Strength.jpg" "https://upload.wikimedia.org/wikipedia/en/5/53/RWS_Tarot_16_Tower.jpg" "https://upload.wikimedia.org/wikipedia/en/9/9b/RWS_Tarot_07_Chariot.jpg" "https://upload.wikimedia.org/wikipedia/en/d/db/RWS_Tarot_06_Lovers.jpg" "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg" "https://upload.wikimedia.org/wikipedia/en/d/de/RWS_Tarot_01_Magician.jpg"])
@@ -24,8 +26,8 @@
   (str "perspective(1500px) rotateX(30deg) rotateY("(/ r 10)"deg) rotateZ("r"deg) scale("s")"))
 
 (defnc app []
-  (let [[gone set-gone] (hks/use-state #{})
-        [props api]
+  (let [[gone set-gone!] (hks/use-state #{})
+        [props {:keys [start!]}]
         (use-springs
          (count cards)
          (fn [i]
@@ -44,36 +46,32 @@
            (let [trigger (> velocity 0.2)
                  dir (if (> xd 0) 1 -1)
                  gone (if (and (not down) trigger) (conj gone index) gone)]
-             (.start
-              api
+             (start!
               (fn [i]
                 (when (= i index)
-                  (clj->js
-                   {:x (if (gone i)
-                         (* (+ 200 (.-innerWidth js/window)) dir)
-                         (if down mx 0))
-                    :rot (+ (/ mx 100)
-                            (if (gone i) (* dir 10 velocity) 0))
-                    :scale (if down 1.1 1)
-                    :delay nil
-                    :config {:friction 50
-                             :tension (if down
-                                        800
-                                        (if (gone i) 200 500))}}))))
-             (set-gone gone)
+                  {:x (if (gone i)
+                        (* (+ 200 (.-innerWidth js/window)) dir)
+                        (if down mx 0))
+                   :rot (+ (/ mx 100)
+                           (if (gone i) (* dir 10 velocity) 0))
+                   :scale (if down 1.1 1)
+                   :delay nil
+                   :config {:friction 50
+                            :tension (if down
+                                       800
+                                       (if (gone i) 200 500))}})))
+             (set-gone! gone)
              (when (and (not down) (= (count gone) (count cards)))
                (js/setTimeout
                 (fn []
-                  (set-gone #{})
-                  (.start
-                   api
+                  (set-gone! #{})
+                  (start!
                    (fn [i]
-                     (clj->js
-                      {:x 0
-                       :y (* i -4)
-                       :scale 1
-                       :rot (- (* (rand) 20) 10)
-                       :delay (* i 100)}))))
+                     {:x 0
+                      :y (* i -4)
+                      :scale 1
+                      :rot (- (* (rand) 20) 10)
+                      :delay (* i 100)})))
                 600))))
          {:axis "x"})]
     (map-indexed
